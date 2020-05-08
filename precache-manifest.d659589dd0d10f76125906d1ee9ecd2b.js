e107e40c703c284dfd8bee201d4d063a6652c4b7
Leonardo Tafarelo <leonardotafarelo@NBQSP-FC450.local>
2020-05-08 13:52:46 -0300

    Updates

---
+++ precache-manifest.d659589dd0d10f76125906d1ee9ecd2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "57c3d4e45f5aba315d955f5338504d01",
+    "url": "/bexs-exam/index.html"
+  },
+  {
+    "revision": "dcbcbe797abae0c5c13a",
+    "url": "/bexs-exam/static/css/2.3712154f.chunk.css"
+  },
+  {
+    "revision": "dcbcbe797abae0c5c13a",
+    "url": "/bexs-exam/static/js/2.3d85a0bd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/bexs-exam/static/js/2.3d85a0bd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "da5d51895d37a405f6c6",
+    "url": "/bexs-exam/static/js/main.84a28a33.chunk.js"
+  },
+  {
+    "revision": "d9c8e5bc9fd9d3cd8b8a",
+    "url": "/bexs-exam/static/js/runtime-main.9336a878.js"
+  }
+]);
